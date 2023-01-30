c1c7f2d7fa88994f9a2e8a76f0b53275e33559b4
Walter Li <li.walter1@gmail.com>
2023-01-30 18:25:02 -0500

    general structure 0.2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,20 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js', // bracket will be replaced by whatever is in entry
+        filename: '[name][contenthash].js', // bracket will be replaced by whatever is in entry with hash
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map', // helps debug
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 8080,
+        open: true, // opens browser automatically
+        hot: true, // hmr
+        compress: true,
+        historyApiFallback: true,
     },
     module: {
         // loaders
@@ -21,8 +34,25 @@ module.exports = {
                     'css-loader',
                     'style.loader',
                 ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
     plugins: [
         new HtmlWebpackPlugin({
